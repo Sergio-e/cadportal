b32c64551acc6bbf2d5fcbcc8d0a1c81c5772640
Sergio <sergio_af92@hotmail.com>
2018-05-12 13:57:13 -0500

    Add devise invitable to collaborators

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -1,5 +1,7 @@
 class Collaborator < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable,
+         :rememberable, :trackable, :validatable,
+         :invitable, invite_for: 2.weeks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_12_180109) do
+ActiveRecord::Schema.define(version: 2018_05_12_185323) do
 
   create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2018_05_12_180109) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
@@ -42,7 +44,21 @@ ActiveRecord::Schema.define(version: 2018_05_12_180109) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer "invitation_limit"
+    t.string "invited_by_type"
+    t.bigint "invited_by_id"
+    t.integer "invitations_count", default: 0
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_collaborators_on_email", unique: true
+    t.index ["invitation_token"], name: "index_collaborators_on_invitation_token", unique: true
+    t.index ["invitations_count"], name: "index_collaborators_on_invitations_count"
+    t.index ["invited_by_id"], name: "index_collaborators_on_invited_by_id"
+    t.index ["invited_by_type", "invited_by_id"], name: "index_collaborators_on_invited_by_type_and_invited_by_id"
     t.index ["reset_password_token"], name: "index_collaborators_on_reset_password_token", unique: true
   end
 
